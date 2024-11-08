5966ca54cee60cd7d32d776ec1ce41fa1d499346
lima-rene <renesilvaab@hotmail.com>
2024-11-07 21:36:50 -0300

    ajusta doc

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,9 +12,9 @@ export class LoginComponent {
 
   users = {
     'rene.silva': { password: 'amou2', name: 'Rene Silva Lima', curso: 'Engenharia de Dados', profilePic: 'foto-rene.jpg', cpf: '450.786.408-94', dne: '2063.00G' },
-    'thiago.santos': { password: 'amogregmaya', name: 'Thiago Nascimento Santos', curso: 'Sistemas de Informação', profilePic: 'foto-thiago.jpeg', cpf: '450.756.408-94', dne: '2063.00G'  },
-    'mari.nogueira': { password: 'amolencinho', name: 'Mariana Felipe Nogueira', curso: 'Engenharia Química', profilePic: 'foto-mari.jpeg', cpf: '470.346.123-54', dne: '2063.00G'  },
-    'neres.neres': { password: 'amosax', name: 'Thais Neres', curso: 'Design', profilePic: 'foto-neres.jpeg', cpf: '395.222.105-94', dne: '2063.00G'  }
+    'thiago.santos': { password: 'amogregmaya', name: 'Thiago Nascimento Santos', curso: 'Sistemas de Informação', profilePic: 'foto-thiago.jpeg', cpf: '475.304.158-16', dne: '2063.00G'  },
+    'mari.nogueira': { password: 'amolencinho', name: 'Mariana Felipe Nogueira', curso: 'Engenharia Química', profilePic: 'foto-mari.jpeg', cpf: '311.111.278-08', dne: '2063.00G'  },
+    'neres.neres': { password: 'amosax', name: 'Thais Matins Neres de Araújo', curso: 'Design', profilePic: 'foto-neres.jpeg', cpf: '444.930.838-75', dne: '2063.00G'  }
   };
 
   constructor(private router: Router) {}
