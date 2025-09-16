db9911bd6e48a5dc2559b3fbb5adc41e547cf9bb
Rene Silva Lima <rene.opzao@gmail.com>
2025-09-16 19:20:10 -0300

    insere gabi

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,7 +14,8 @@ export class LoginComponent {
     'rene.silva': { password: 'amou2', name: 'Rene Silva Lima', curso: 'Engenharia de Dados', profilePic: 'foto-rene.jpg', cpf: '450.786.408-94', dne: '2063.00G' },
     'thiago.santos': { password: 'amogregmaya', name: 'Thiago Nascimento Santos', curso: 'Sistemas de Informação', profilePic: 'foto-thiago.jpeg', cpf: '475.304.158-16', dne: '2063.00G'  },
     'mari.nogueira': { password: 'amolencinho', name: 'Mariana Felipe Nogueira', curso: 'Engenharia Química', profilePic: 'foto-mari.jpeg', cpf: '311.111.278-08', dne: '2063.00G'  },
-    'neres.neres': { password: 'amosax', name: 'Thais Matins Neres de Araújo', curso: 'Design', profilePic: 'foto-neres.jpeg', cpf: '444.930.838-75', dne: '2063.00G'  }
+    'neres.neres': { password: 'amosax', name: 'Thais Matins Neres de Araújo', curso: 'Design', profilePic: 'foto-neres.jpeg', cpf: '444.930.838-75', dne: '2063.00G'  },
+    'gabi.rosch': { password: 'amobjork', name: 'Gabriela Rosch Parraguez', curso: 'Gastronomia', profilePic: 'foto-gabi.jpeg', cpf: '513.011.158-35', dne: '2063.00G'  }
   };
 
   constructor(private router: Router) {}
